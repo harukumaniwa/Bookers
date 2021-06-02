24a6cdbf9262c1d90e04691b3f47e3a8b1827d4b
harukumaniwa <maniwa.haruku.infratop@gmail.com>
2021-06-02 14:27:58 +0000

    Finish CRUD

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,42 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+    @posts = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to books_path(book)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path(book)
+  end
+  
+  private
+  
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+
+end
