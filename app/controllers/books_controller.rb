3fa958f9ed9a0ddbc92b8da423b38f6fbccd87fa
harukumaniwa <maniwa.haruku.infratop@gmail.com>
2021-06-03 01:23:55 +0000

    error

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
     @book = Book.new
+
     @posts = Book.all
   end
 
@@ -10,31 +11,37 @@ class BooksController < ApplicationController
 
   def new
   end
-  
+
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    if book.save
+      flash[:created] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      render "index"
+    end
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to books_path(book)
+    if book.update(book_params)
+      flash[:update] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path(book)
   end
-  
+
   private
-  
+
     def book_params
       params.require(:book).permit(:title, :body)
     end
